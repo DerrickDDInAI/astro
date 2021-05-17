0abb4fa1ff8b86c7a7c2fade22b6caecc470746d
Derrick DD Van Frausum <derrick.vanfrausum@gmail.com>
2021-05-18 00:41:50 +0200

    threading

---
+++ core/build_db.py
@@ -0,0 +1,144 @@
+"""
+Script to
+1. read all light curves
+2. extract their features
+3. export the dataset to a csv file
+"""
+
+# =====================================================================
+# Import
+# =====================================================================
+
+# Import internal modules
+import os
+import time
+from threading import Thread, RLock
+from typing import List
+
+# Import 3rd party modules
+import pandas as pd
+import numpy as np
+
+# Import local modules
+from process_lightcurve import read_lightcurve
+
+# =====================================================================
+# Classes
+# =====================================================================
+
+# Instantiate RLock object
+writing_csv_lock = RLock()
+
+# Define child class of Thread
+
+
+class SyncThread(Thread):
+    """
+    Child class of thread
+    to read the light curves in threads
+    """
+
+    def __init__(self, lc_list, tce_table):
+        """
+        Function to create an instance of SyncThread class
+        """
+        Thread.__init__(self)
+        self.lc_list = lc_list
+        self.tce_table = tce_table
+
+    def run(self):
+        """
+        Function to start the thread
+        """
+        # Track time to run the thread
+        start_time = time.time()
+        print(f"Starting thread")
+        
+        # create empty DataFrame
+        df_thread = pd.DataFrame(columns=['flux', 'kepid', 'best_fit_period', 'av_training_set'])
+
+        # for filepath in list of light curves for the thread instance
+        for filepath in self.lc_list:
+            
+            # read light curve file, preprocess and convert into DataFrame
+            lc_df = read_lightcurve(filepath, self.tce_table)
+
+            # append DataFrame to `df`DataFrame
+            df_thread = df_thread.append(lc_df)
+
+        # compute thread runtime
+        end_time = time.time()
+        print(end_time - start_time)
+
+        # Export dataframe to csv
+        with writing_csv_lock:
+            print(f"writing light curves to csv")
+
+            # If file exists, don't repeat writing the header
+            file_exists = os.path.isfile(os.path.join(
+                "assets", "data", "Kepler", "thread_csv_all.csv"))
+            if file_exists:
+                df_thread.to_csv(os.path.join(
+                    "assets", "data", "Kepler", "thread_csv_all.csv"), header=False, index=False, mode='a')
+            else:
+                df_thread.to_csv(os.path.join(
+                    "assets", "data", "Kepler", "thread_csv_all.csv"), header=True, index=False, mode='a')
+
+
+# =====================================================================
+# Run
+# =====================================================================
+
+run_script: bool = True
+
+# Check if file it exists
+file_exists = os.path.isfile(os.path.join(
+    "assets", "data", "Kepler", "thread_csv_all.csv"))
+if file_exists:
+    # For safety reasons, ask the user if he's sure to run the script
+    delete_or_not: str = input("""
+    The csv file already exists.
+    This script will replace the current csv file.
+    Do you still want to run it? (yes/no): 
+    """).lower()
+    if delete_or_not in ("yes", "y"):
+        os.remove(os.path.join("assets", "data", "thread_csv_all.csv"))
+    else:
+        run_script = False
+
+if run_script:
+
+    # get kepler tce DataFrame
+    csv_path = "/Users/derrickvanfrausum/BeCode_AI/git-repos/astro/core/assets/data/Kepler/q1_q17_dr24_tce_2021.05.17_09.38.15.csv"
+    kepler_tce = pd.read_csv(csv_path, comment="#")
+
+    # create empty lists
+    lc_list = []
+    threads = []
+
+    # get all light curve file paths in downloaded_lc folder
+    folder_path = os.path.abspath('assets/data/Kepler/downloaded_lc')
+
+    for path, dirs, files in os.walk(folder_path):
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            lc_list.append(filepath)
+
+    # slice light curves files list in 5 sets (1 for each thread)
+    splitted_list = np.array_split(lc_list, 5)
+
+    # create thread for each set
+    for array in splitted_list:
+        thread = SyncThread(list(array), kepler_tce)
+        
+        # Add thread to thread list
+        threads.append(thread)
+    
+    # Launch threads
+    for thread in threads:
+        thread.start()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+    print("Exiting Main Thread")
---
+++ core/process_lightcurve.py
@@ -0,0 +1,121 @@
+"""
+Helper functions to preprocess light curves
+"""
+
+# =====================================================================
+# Import
+# =====================================================================
+
+# Import internal modules
+import os
+
+# from typing import List
+
+# Import 3rd party modules
+import pandas as pd
+import numpy as np
+import lightkurve as lk
+
+# Import local modules
+
+
+# =====================================================================
+# Functions
+# =====================================================================
+
+def download_lightcurve(target_name: str) -> lk.lightcurve.TessLightCurve:
+    """
+    Function to download the light curve of a target.
+    In case the search returns multiple data products:
+    it downloads the data with biggest exposure time and most recently observed
+    """
+    # search for light curves
+    search_result = lk.search_lightcurve(target_name)
+
+    try:
+        # get max Exposure time
+        exptime_max = np.max(search_result.exptime)
+
+        # get data products with biggest exposure time
+        exptime_max_result = search_result[search_result.exptime == exptime_max]
+
+        # get index of most recent observation
+        most_recent_idx = np.argmax(exptime_max_result.year)
+
+        # get data product
+        return exptime_max_result[most_recent_idx].download()
+    except:
+        return None
+
+# def get_lightcurve_list(target_list: list) -> pd.DataFrame:
+#     """
+#     Function to download lightcurves from a list of targets
+#     """
+#     # create empty list
+#     lc_list: list = []
+
+#     for target in target_list:
+
+#         # download lightcurve if found
+#         lc = download_lightcurve(target)
+
+#         # if lightcurve downloaded
+#         if lc:
+
+#             # preprocess lc
+#             pipeline_lightcurve
+
+#             # append to list
+#             lc_list.append(lc)
+    
+#     return lc_df
+
+def pipeline_lightcurve(lc):
+    """
+    function to preprocess 
+    * a light curve
+    """
+    # remove missing values & outliers
+    lc = lc.remove_nans().remove_outliers()
+    
+    # flatten lc
+    flat_lc = lc.flatten()
+
+    # compute best fit period
+    periodogram = flat_lc.to_periodogram(method="bls", period=np.arange(0.5, 10, 0.001))
+    best_fit_period = periodogram.period_at_max_power
+
+    # fold lc according to best fit period
+    folded_lc = flat_lc.fold(period=best_fit_period)
+
+    # return a tuple
+    return (folded_lc, best_fit_period)
+
+
+def read_lightcurve(filepath, tce_table) -> pd.DataFrame:
+    """
+    Function to read light curve stored locally
+    and preprocess it.
+    """
+    # read light curve file
+    lc = lk.read(filepath)
+
+    # preprocess light curve
+    processed_lc, best_fit_period = pipeline_lightcurve(lc)
+
+    # Convert into DataFrame
+    lc_df = processed_lc.to_pandas()[["flux"]]
+
+    # remove any missing values that would remain (normally removed during preprocessing)
+    lc_df.dropna(inplace=True)
+
+    # add target ID to dataframe
+    lc_df["kepid"] = processed_lc.meta['TARGETID']
+
+    # add best fit period to dataframe
+    lc_df["best_fit_period"] = best_fit_period
+
+    # merge dataframe with tce dataset (mainly to add classification variable)
+    lc_df = lc_df.merge(tce_table[["kepid", "av_training_set"]], on="kepid")
+
+    return lc_df
